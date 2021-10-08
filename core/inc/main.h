7291cd0c45a89411655b718f5d709c62c1e4d829
morgan pigeron <m.pigeron@hotmail.fr>
2021-10-08 21:52:36 +0200

    delay second

--- core/inc/main.h
+++ core/inc/main.h
@@ -2,3 +2,9 @@
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+#define LOG_LEVEL_LOCAL ESP_LOG_VERBOSE
+#include "esp_log.h"
+#define LOG_TAG "MAIN"
+
+#define pdSECOND pdMS_TO_TICKS(1000)
--- core/src/main.cpp
+++ core/src/main.cpp
@@ -1,14 +1,10 @@
 #include "main.h"
 
-#define LOG_LEVEL_LOCAL ESP_LOG_VERBOSE
-#include "esp_log.h"
-#define LOG_TAG "MAIN"
-
 extern "C" void app_main(void) 
 {
   while(true)
   {
     ESP_LOGI(LOG_TAG, "Hello World");
-    vTaskDelay(1000);
+    vTaskDelay(pdSECOND);
   }
 }
